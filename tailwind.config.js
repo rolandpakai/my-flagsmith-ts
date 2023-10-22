1edc50ab826bf2edeefe5cd75f8b666cd5ffd8e4
roland.pakai <roland.pakai@lightware.com>
2023-10-22 11:08:33 +0200

    Added flowbite support to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./src/**/*.{js,ts}', './public/index.html'],
+  content: [
+    './src/**/*.{js,ts}',
+    './public/index.html',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
