172e9d0e6988de160ed99ea0f1d1b65da0d5205d
roland.pakai <roland.pakai@lightware.com>
2023-10-22 09:00:57 +0200

    Add flagsmith code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,51 @@
-// Import helpers.
-import { setMessage } from '@/helpers/set-message';
+'use strict';
 
-/**
- * Will find #root element and set HTML to "Hello World!".
- */
-function editDom(): void {
-  const root = document.getElementById('root'); // Get root element.
-  if (root) {
-    root.innerHTML = `<p>${setMessage()}</p>`; // Set html of the root element.
-  }
-}
+import flagsmith from 'flagsmith';
+import type {
+  IFlags,
+  IRetrieveInfo,
+  LoadingState,
+} from './@types/flagsmith.types';
 
-editDom(); // Call editDom.
+const environmentID = 'DCYBQgBcuRF86fmvgZc2os';
+const feature_name = 'test_feature';
+
+flagsmith.init({
+  environmentID,
+  cacheFlags: false,
+  // cacheOptions: {ttl:0, skipAPI:false},
+  // enableLogs: true,
+  // enableAnalytics: true,
+  identity: 'flagsmith_sample_user',
+  onChange: (
+    oldFlags: IFlags | null,
+    params: IRetrieveInfo,
+    loadingState: LoadingState,
+  ) => {
+    console.log('oldFlags: ' + JSON.stringify(oldFlags));
+    console.log('params: ' + JSON.stringify(params));
+    console.log('loadingState: ' + JSON.stringify(loadingState));
+
+    const flags = flagsmith.getAllFlags();
+    console.log('Received flags', flags);
+
+    const has_feature = flagsmith.hasFeature(feature_name);
+
+    console.log('has_feature: ' + has_feature);
+
+    if (has_feature) {
+      const value = flagsmith.getValue(feature_name);
+      console.log('value = ' + value);
+
+      if (oldFlags) {
+        // Check whether value has changed
+        const value_old =
+          oldFlags[feature_name] && oldFlags[feature_name].value;
+
+        if (value !== value_old) {
+          // Value has changed, do something here
+        }
+      }
+    }
+  },
+});
