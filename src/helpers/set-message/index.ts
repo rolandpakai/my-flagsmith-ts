c93483193a8b0eeb8ea4b6bc798d6343a3dd574c
roland.pakai <roland.pakai@lightware.com>
2023-10-22 09:00:37 +0200

    Remove not used helper folder

--- src/helpers/set-message/index.ts
+++
@@ -1,9 +0,0 @@
-/** * Will return a welcome message using the `name` parameter.
- *
- * @param [name="You"] Any name.
- *
- * @return The welcome message.
- */
-export function setMessage(name = 'You'): string {
-  return `Hello World and ${name}!`;
-}
